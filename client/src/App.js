acd369871cb2774572dfb4440336f89524b752ee
Uuganjargal <uuganjargal@erdenetmc.mn>
2021-05-04 17:10:00 +0800

    Changed socket endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 //import {SocketContext, socket} from 'socket';
 import socketio from "socket.io-client";
 
-const ENDPOINT = "http://127.0.0.1/";
+const ENDPOINT = "/";
 
 function App() {
   const [socket, setSocket] = useState(undefined);
